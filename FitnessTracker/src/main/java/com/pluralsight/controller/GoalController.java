889de1471b946dde92cd044e9a4a6dbe358f62be
Ramon Lacava <ramonrune@gmail.com>
2019-01-01 15:25:45 -0200

    Validation

--- FitnessTracker/src/main/java/com/pluralsight/controller/GoalController.java
+++ FitnessTracker/src/main/java/com/pluralsight/controller/GoalController.java
@@ -1,7 +1,10 @@
 package com.pluralsight.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,8 +27,17 @@ public class GoalController {
 	}
 	
 	@RequestMapping(value = "/addGoal", method = RequestMethod.POST)
-	public String addGoal(@ModelAttribute("goal") Goal goal) {
+	public String addGoal(@Valid @ModelAttribute("goal") Goal goal, BindingResult result) {
+		
+		System.out.println("Result has errors: " + result.hasErrors());
+		
 		System.out.println("Minutes: " + goal.getMinutes());
+		
+		
+		if(result.hasErrors()) {
+			return "addGoal";
+		}
+		
 		return "redirect:addMinutes.html";
 	}
 }
--- FitnessTracker/src/main/webapp/WEB-INF/jsp/addGoal.jsp
+++ FitnessTracker/src/main/webapp/WEB-INF/jsp/addGoal.jsp
@@ -6,14 +6,32 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Add Goal</title>
+
+<style>
+	.error{
+		color: #ff0000;
+	}
+	
+	.error-block{
+		color: #000;
+		background-color: #ffeeee;
+		border: 3px solid #ff0000;
+		padding: 8px;
+		margin: 16px;
+	}
+</style>
 </head>
 <body>
 	<form:form commandName="goal">
+		<form:errors path="*" cssClass="error-block" element="div"/>
 		<table>
 			<tr>
-				<td>Enter Minutes: </td>
+				<td>Enter Minutes: </td>  
 				<td>
-					<form:input path="minutes"/>
+					<form:input path="minutes" cssErrorClass="error"/>
+				</td>
+				<td>
+					<form:errors path="minutes" cssClass="error"/>
 				</td>
 			</tr>
 			<tr>
